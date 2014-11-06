7ca693de9eb4f48c2af033a617c00f65dbc7339a
Rafael Mahnovetsky <rmahnovetsky@gmail.com>
2014-11-06 20:24:46 +1100

    clean up

--- db/migrate/20141106081345_fix_column_name.rb
+++ db/migrate/20141106081345_fix_column_name.rb
@@ -2,7 +2,5 @@ class FixColumnName < ActiveRecord::Migration
   def change
     rename_column :transactions, :fequency, :frequency
     rename_column :properties, :purcahse_date, :purchase_date
-    rename_column :transactions, :outgoing, :outgoing_type
-
   end
 end
