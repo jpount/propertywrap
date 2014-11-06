a1c88b180cce141023d29ea1210e68fba6cc8c34
vagrant <vagrant@wrap-inno>
2014-11-06 20:41:35 +0000

    Fixed auth

--- app/controllers/property_details_controller.rb
+++ app/controllers/property_details_controller.rb
@@ -1,11 +1,16 @@
 class PropertyDetailsController < ApplicationController
-  before_action :set_oauth_property
+  before_action :set_property
   before_action :set_detail_property, only: [:show, :image, :index]
 
   respond_to :html, :json
 
   require 'rest_client'
   require 'json'
+
+  def set_property
+    # Auth Token generated from RP Data OAuth2
+    @auth_token = 'eyJhbGciOiJSUzI1NiJ9.eyJleHAiOiIxNDE1MzQ5MDYwNTI4Iiwic2NvcGUiOiJTVFMgQ1JUIFBUWSBTR1QgVFRMIE1BUCIsInJwLnNydiI6IjI1NTU0MTc2OTM2MDIiLCJpc3MiOiJSUERhdGEub2F1dGgyIiwicnAuY2xpIjoiMGYyZGI4ZGEifQ.UfBJzqdm3FYssNUfHxoQ-c7ZMdAGZodFnVVkfsrHWLgBJWZ0R6iSymH3JWARqjnpExDv9Wxho6QpUUEns4_zN4l_OsWRb68pO6Ted5aqJiBzPfklRa8t4T7fDaHsMTndmCd5KBqrZi2xt61kUK7yBlOVx7bN-dqmOhtwM6l0-TQ'
+  end
 
   def index
 #    @address = '1 Shelley Street, Sydney NSW 2000'
