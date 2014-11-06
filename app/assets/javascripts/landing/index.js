2d1edf1e269bdd73c77360ce33e0d1a46c38cf91
jpount <johnpount@gmail.com>
2014-11-07 01:26:08 +1100

    homepage

--- app/assets/javascripts/landing/index.js
+++ app/assets/javascripts/landing/index.js
@@ -17,29 +17,28 @@
 
         $scope.info = [
             {
-                label: 'Income', amount: '$40,000'
+                label: 'Income', amount: '$40,000', highlightItem: false
             },
             {
-                label: 'Outflow', amount: '$40,000'
+                label: 'Outflow', amount: '$50,000', highlightItem: false
             }];
 
         $scope.propertyList = [{
             label: '1 Shelley St, Sydney NSW', yield: '10%', description: ['Last rent paid on 3/2/2014'], outstanding: 0,
-            image: 'images/house1.jpeg'
+            image: '/assets/house1.jpeg'
         },
             {
                 label: '16 Martin Pl, Sydney NSW', yield: '4%', description: ['Last rent paid on 3/2/2014', 'Maintenence required', 'Outstanding rent for April'], outstanding: 2,
-                image: 'images/house2.jpeg'
+                image: '/assets/house2.jpeg'
             },
             {
                 label: '23 Foveaux, Perth WA', yield: '13%', description: ['Outstanding rent for April'], outstanding: 1,
-                image: 'images/house3.jpeg'
+                image: '/assets/house3.jpeg'
             },
             {
                 label: '50 Sunshine St, Gold Coast ', yield: '13%', description: ['Last rent paid on 3/2/2014'], outstanding: 0,
-                image: 'images/house4.jpeg'
-            },
-        ];
+                image: '/assets/house4.jpeg'
+            }];
 
         $scope.options = {thickness: 20};
 
@@ -54,6 +53,38 @@
             {icon: 'line-chart', title: 'Macquarie Wrap', description: 'Directly manage other investments'},
             {icon: 'money', title: 'Macquarie Loan', description: 'Get your home loan approved instantly'}
         ];
+        var video1 = document.getElementById("video1");
+        video1.autoplay = false;
+        var video2 = document.getElementById("video2");
+        video2.autoplay = false;
+        var video3 = document.getElementById("video3");
+        video3.autoplay = false;
+        video1.addEventListener('ended',function(){
+            $scope.welcomeHighlight = false;
+            $scope.info[0].highlightItem = true;
+            $scope.$apply();
+            video2.play();
+        },false);
+        video2.addEventListener('ended',function(){
+            $scope.info[0].highlightItem = false;
+            $scope.info[1].highlightItem = true;
+            $scope.$apply();
+            video3.play();
+        },false);
+        video3.addEventListener('ended',function(){
+            $scope.info[1].highlightItem = false;
+            $scope.$apply();
+        },false);
+
+        $scope.test = function(){
+            video1.play();
+            $scope.welcomeHighlight = true;
+        };
+
+        $scope.opposite = function(){
+            $scope.info[0].highlightItem = false;
+            $scope.info[1].highlightItem = false;
+        };
 
         $document.on('scroll', function($scope){
 
